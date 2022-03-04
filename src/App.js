a7184d373d50d13223f6c6fc5d26e78ce4d23dbc
shaun <1600083715@qq.com>
2022-03-04 23:37:15 +0800

    Header finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react"
 import "./App.css"
+import Header from "./components/header"
+import InputForm from "./components/inputForm"
+import OutputForm from "./components/outputForm"
 
 const App = () => (
   <div className="App">
-    
+    <Header />
+    <InputForm />
+    <OutputForm />
   </div>
 )
 
